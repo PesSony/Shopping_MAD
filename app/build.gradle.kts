c58028ee1806bc524e5dd03f5c157c1e9ee43ecf
SSaa2711 <dragonboii2711@gmail.com>
2023-12-24 17:39:56 +0700

    Add Login and Register with FireBase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -43,6 +44,11 @@ android {
 
 dependencies{
 
+
+    implementation("com.google.firebase:firebase-auth")
+    implementation("com.google.android.gms:play-services-auth:20.7.0")
+    implementation(platform("com.google.firebase:firebase-bom:32.7.0"))
+    implementation("com.google.firebase:firebase-analytics")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
---
+++ app/src/main/java/com/example/maproject/View/Activity/ActivityLogin.kt
@@ -0,0 +1,161 @@
+package com.example.maproject.View.Activity
+
+import android.content.Intent
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
+import android.util.Patterns
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import com.example.maproject.R
+import com.example.maproject.databinding.LoginFromActivityBinding
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+
+class ActivityLogin : AppCompatActivity() {
+    private var binding: LoginFromActivityBinding? = null
+    private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var googleSignInClient: GoogleSignInClient
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = LoginFromActivityBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        binding?.loginButton?.setOnClickListener {
+            val email = binding?.loginEmail?.text.toString()
+            val password = binding?.loginPw?.text.toString()
+
+            val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.login_with_google))
+                .requestEmail()
+                .build()
+            googleSignInClient = GoogleSignIn.getClient(this, gso)
+
+            if (email.isNotEmpty() && password.isNotEmpty()) {
+                firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        val intent = Intent(this, MainActivityKotlin::class.java)
+                        startActivity(intent)
+                    } else {
+                        Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+                    }
+                }
+            } else {
+                Toast.makeText(this, "Fields cannot be empty", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        binding?.facebook?.setOnClickListener {
+            val facbookB = Intent(this, MainActivityKotlin::class.java)
+            startActivity(facbookB)
+        }
+
+
+        binding?.forgotPassword?.setOnClickListener {
+            val builder = AlertDialog.Builder(this)
+            val view = layoutInflater.inflate(R.layout.dialog_forgot, null)
+            val userEmail = view.findViewById<EditText>(R.id.editBox)
+            builder.setView(view)
+            val dialog = builder.create()
+            view.findViewById<Button>(R.id.btnReset).setOnClickListener {
+                compareEmail(userEmail)
+                dialog.dismiss()
+            }
+            view.findViewById<Button>(R.id.btnCancel).setOnClickListener {
+                dialog.dismiss()
+            }
+            if (dialog.window != null) {
+                dialog.window!!.setBackgroundDrawable(ColorDrawable(0))
+            }
+            dialog.show()
+        }
+        binding?.signupRedirectText?.setOnClickListener {
+            val signupIntent = Intent(this, SignupActivity::class.java)
+            startActivity(signupIntent)
+        }
+        binding?.google?.setOnClickListener { signInWithGoogle() }
+
+    }
+
+
+    private fun signInWithGoogle() {
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.login_with_google))
+            .requestEmail()
+            .build()
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+
+        val signInIntent = googleSignInClient.signInIntent
+        launcher.launch(signInIntent)
+    }
+
+    private val launcher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == RESULT_OK) {
+                val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+                handleResults(task)
+            }
+            val intentB = Intent(this, MainActivityKotlin::class.java)
+            startActivity(intentB)
+        }
+
+    private fun handleResults(task: Task<GoogleSignInAccount>) {
+        if (task.isSuccessful) {
+            val account: GoogleSignInAccount? = task.result
+            if (account != null) {
+                // Google Sign-In successful, authenticate with Firebase
+                updateUI(account)
+//                val  intentB = Intent(this,MainActivity::class.java)
+//                startActivity(intentB)
+            }
+        } else {
+            // Sign-In failed, show an error message
+            Toast.makeText(this, "Sign-In failed. Please try again.", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun updateUI(account: GoogleSignInAccount) {
+        val credential = GoogleAuthProvider.getCredential(account.idToken, null)
+        firebaseAuth.signInWithCredential(credential).addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                // Firebase authentication successful, start the MainActivity or perform other actions
+                val mainIntent = Intent(this, MainActivityKotlin::class.java)
+                startActivity(mainIntent)
+            } else {
+                // Firebase authentication failed, show an error message
+                Toast.makeText(
+                    this,
+                    "Unable to log in at the moment. Please try again later.",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+    }
+
+    //Outside onCreate
+    private fun compareEmail(email: EditText) {
+        if (email.text.toString().isEmpty()) {
+            return
+        }
+        if (!Patterns.EMAIL_ADDRESS.matcher(email.text.toString()).matches()) {
+            return
+        }
+        firebaseAuth.sendPasswordResetEmail(email.text.toString())
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    Toast.makeText(this, "Check your email", Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+}
